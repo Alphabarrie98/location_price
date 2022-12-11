0d200d01348099266d61eb33b4f9e571e8af306b
Alphabarrie98 <barriealpha24@gmail.com>
2022-12-11 23:35:48 +0000

    initial commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,11 @@ import React from "react";
 export default function Navbar() {
     return (
 
-        <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+        <nav class="bg-white border-blue-200 px-2 sm:px-4 py-2.5 rounded dark:bg-blue-900">
             <div class="container flex flex-wrap items-center justify-between mx-auto">
-                <a href="https://flowbite.com/" class="flex items-center">
-                    <img src="https://flowbite.com/docs/images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
-                    <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                <a href="/" class="flex items-center">
+                    <img src="https://i.pinimg.com/564x/fb/b4/1b/fbb41b9220b12bbd7d672e19b098769a.jpg" class="h-6 mr-3 sm:h-9" alt="Mont" />
+                    <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Locate Montserrado</span>
                 </a>
                 <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
                     <span class="sr-only">Open main menu</span>
